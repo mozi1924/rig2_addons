ecc4b4014b7e91f3e6d861fc1093b25ce08c782c
mozi1924 <mozi1924@arasaka.ltd>
2026-06-24 19:25:43 +0800

    chore: bump version to 1.1.9

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Rig2",
     "author": "mozi1924",
-    "version": (1, 1, 8),
+    "version": (1, 1, 9),
     "blender": (4, 5, 0),
     "location": "Properties > Data, 3D View > Side Panel",
     "description": "A modular binding tool for Rig2 Armatures with Face/MoCap support ready",
